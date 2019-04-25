f20e08c6885976961c3e3aba0dc137f9bb9c4dc7
errolgh <ewatsonrei@gmail.com>
2019-04-25 17:07:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 class Application < Sinatra::Base
-  # Write your code here!
+#our application always inherets sinatra's base libary
+#sinatra simplifies our routes for use with verb-do-end blocks where we then specify what erb file we want to use in views (index.erb in this case...)
+#this is a controller...
+  get '/' do
+    erb :index
+  end
 
+
+  post '/greet' do
+    erb :greet
+  end
 end
+# '/' is the home page, we'll use index.erb later on in its own file
